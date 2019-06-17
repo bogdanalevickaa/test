6b412bcc9347b9fd5f9c9ea118d9ceca13d605f5
dana <bogdanalevickaa@gmail.com>
2019-06-17 19:06:15 +0300

    add

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$('.slider').slick({
+	dots:true,
+	infinite : false,
+	// autoplay:true,
+	// autoplaySpeed:2000,
+})
+$('document').ready(function(){
+	$('#country_name').click(function(){
+	 $('.countries').fadeToggle();
+	});
+
+	$('.option').on('click', function() {
+		var counrty = this.value;
+		$('#country_name').val(counrty);
+		$('.countries').hide();
+	});
+
+});
